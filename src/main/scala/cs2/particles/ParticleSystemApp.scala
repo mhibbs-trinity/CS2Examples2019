2194a83c49f3ba63a5eda3bf6e9713b2b0397ca6
2012-06-28 <mhibbs@titan22.cs.trinity.edu>
2019-02-11 15:22:46 -0600

    added image particle system stuff

--- src/main/scala/cs2/particles/ParticleSystemApp.scala
+++ src/main/scala/cs2/particles/ParticleSystemApp.scala
@@ -9,41 +9,38 @@ import scalafx.scene.paint.Color
 import scalafx.scene.input.MouseEvent
 import scalafx.Includes._
 
-object ParticleSystemApp {
-  
-  val app = new JFXApp {
-    stage = new JFXApp.PrimaryStage {
-      title = "Particles!"
-      scene = new Scene(600,400) {
-        val canvas = new Canvas(600,400)
-        val g = canvas.graphicsContext2D
-        content = canvas
-        
-        var systems = List[ParticleSystem]()
-        
-        canvas.onMouseClicked = (e:MouseEvent) => {
-          systems ::= new RainbowParticleSystem(new Vec2(e.x, e.y))
-        }
-        
-        val bg = new RainbowBackground(600, 400)
-        
-        val timer = AnimationTimer(t => {
+object ParticleSystemApp extends JFXApp {
+
+  stage = new JFXApp.PrimaryStage {
+    title = "Particles!"
+    scene = new Scene(600, 400) {
+      val canvas = new Canvas(600, 400)
+      val g = canvas.graphicsContext2D
+      content = canvas
+
+      var systems = List[ParticleSystem]()
+
+      canvas.onMouseClicked = (e: MouseEvent) => {
+        systems ::= new ImageParticleSystem(new Vec2(e.x, e.y))
+      }
+
+      val bg = new RainbowBackground(600, 400)
+
+      var prevT:Long = 0L
+      val timer = AnimationTimer(t => {
+        if(t - prevT > 1e9/60) {
+          prevT = t
+          
           bg.display(g)
-          for(sys <- systems) {
+          for (sys <- systems) {
             sys.display(g)
             sys.timeStep()
             sys.addParticle()
-            sys.applyForce(new Vec2(0,0.1))
+            sys.applyForce(new Vec2(0, 0.1))
           }
-        })
-        timer.start
-      }
+        }
+      })
+      timer.start
     }
   }
-  
-  def main(args:Array[String]):Unit = {
-    app.main(args)
-  }
-  
-  
 }
