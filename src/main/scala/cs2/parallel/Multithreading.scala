d60a40ac07db2c4c7e7e6d101f96547a2dbf5829
2012-06-28 <mhibbs@titan22.cs.trinity.edu>
2019-04-12 15:22:18 -0500

    Continued multithreading examples

--- src/main/scala/cs2/parallel/Multithreading.scala
+++ src/main/scala/cs2/parallel/Multithreading.scala
@@ -24,9 +24,62 @@ object Multithreading {
     name
   }
   
+  def betterCountName():String = {
+    var haveAnswer = false
+    var timesUp = false
+    
+    val thread = new java.lang.Thread {
+      override def run() {
+        var ctr = 5
+        while(!haveAnswer && ctr > 0) {
+          println(ctr)
+          ctr -= 1
+          java.lang.Thread.sleep(1000)
+        }
+        if(!haveAnswer) {
+          println("Times's up")
+          timesUp = true
+        }
+      }
+    }
+    thread.start
+    
+    var name:String = null
+    println("Enter your name: ")
+    while(!timesUp && !Console.in.ready) {
+      //java.lang.Thread.sleep(10)
+    }
+    if(!timesUp) {
+      name = readLine
+      haveAnswer = true
+    }
+    name
+  }
+  
+  class Lock
+  
+  def simpleThreadCount():Unit = {
+    val lock = new Lock
+    
+    var counter = 0
+    val threads = Array.fill(10)(new java.lang.Thread {
+      override def run() {
+        for(i <- 1 to 1000000/10) {
+          lock.synchronized {
+            counter += 1 
+          }
+        }
+      }
+    })
+    threads.foreach(_.start)
+    threads.foreach(_.join)
+    println(counter)
+  }
+  
   def main(args:Array[String]):Unit = {
-    val n = countDownGetName
-    println("Hello, " + n)
+    simpleThreadCount
+    //val n = betterCountName
+    //println("Hello, " + n)
   }
   
   
