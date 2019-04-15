61d30673dfe910de01f973caa0cc19fe74cf6bf0
2012-06-28 <mhibbs@titan22.cs.trinity.edu>
2019-04-15 15:23:36 -0500

    Continued multithreading examples

--- src/main/scala/cs2/parallel/Multithreading.scala
+++ src/main/scala/cs2/parallel/Multithreading.scala
@@ -76,8 +76,103 @@ object Multithreading {
     println(counter)
   }
   
+  val lock1 = new Lock
+  val lock2 = new Lock
+  
+  import java.lang.Thread
+  
+  def funcA = {
+    lock1.synchronized {
+      println("A1")
+      Thread.sleep(1)
+      lock2.synchronized {
+        println("A2")
+      }
+    }
+  }
+  def funcB = {
+    lock2.synchronized {
+      println("B2")
+      Thread.sleep(1)
+      lock1.synchronized {
+        println("B1")
+      }
+    }
+  }
+  
+  def tryToDeadlock():Unit = {
+    val athreads = Array.fill(10)( new Thread {
+      override def run() { funcA }
+    })
+    val bthreads = Array.fill(10)( new Thread {
+      override def run() { funcB }
+    })
+    athreads.foreach(_.start)
+    Thread.sleep(10)
+    bthreads.foreach(_.start)
+  }
+  
+  def simpleWaitNotify:Unit = {
+    var turn = -1
+    val threads = Array.tabulate(8)(idx => new Thread {
+      override def run() {
+        println("Start thread " + idx)
+        for(ctr <- 1 to 10) lock1.synchronized {
+          while(turn != idx) { lock1.wait }
+          Thread.sleep((math.random() * 100).toInt)
+          println("Thread " + idx + " iteration " + ctr)
+          turn = (turn + 1) % 8
+          lock1.notifyAll
+        }
+      }
+    })
+    threads.foreach(_.start)
+    Thread.sleep(500)
+    turn = 0
+    lock1.synchronized { lock1.notifyAll }
+  }
+  
+  //Factorials
+  def factRecur(n:BigInt):BigInt = {
+    if(n <= 1) 1
+    else n * factRecur(n-1)
+  }
+  def factFor(n:BigInt):BigInt = {
+    var product:BigInt = 1
+    val one:BigInt = 1
+    for(x <- one to n) {
+      product *= x
+    }
+    product
+  }
+  def factCollect(n:BigInt):BigInt = {
+    val one:BigInt = 1
+    (one to n).reduce(_*_) //.product
+  }
+  
+  import java.util.concurrent._
+  def factExec(n:BigInt):BigInt = {
+    val service = Executors.newCachedThreadPool
+    val future1 = service.submit(new Callable[BigInt] {
+      def call:BigInt = {
+        (BigInt(1) until n/2).product  
+      }
+    })
+    val future2 = service.submit(new Callable[BigInt] {
+      def call:BigInt = {
+        (n/2 to n).product  
+      }
+    })
+    val res = future1.get * future2.get
+    service.shutdown
+    res
+  }
+  
   def main(args:Array[String]):Unit = {
-    simpleThreadCount
+    println(factExec(50000))
+    //simpleWaitNotify
+    //tryToDeadlock
+    //simpleThreadCount
     //val n = betterCountName
     //println("Hello, " + n)
   }
