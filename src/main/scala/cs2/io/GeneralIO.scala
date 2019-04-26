0c30b6c17732a691fae5be2cdb05b29d94460090
2012-06-28 <mhibbs@titan22.cs.trinity.edu>
2019-04-26 15:30:51 -0500

    More generalized Input/Output streams

--- src/main/scala/cs2/io/GeneralIO.scala
+++ src/main/scala/cs2/io/GeneralIO.scala
@@ -5,26 +5,47 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.BufferedOutputStream
 import java.io.DataOutputStream
+import java.io.DataInputStream
+import java.io.BufferedInputStream
+import java.io.OutputStream
+import java.io.InputStream
 
 object GeneralIO {
   
+  def copyStream(in:InputStream, out:OutputStream):Unit = {
+    while(in.available() > 0) {
+      out.write(in.read)
+    }
+  }
+  
   def main(args:Array[String]):Unit = {
+    for(str <- args) println(str)
     
     val fis = new FileInputStream(new File("tempest.txt"))
     val fos = new FileOutputStream(new File("tempestCopy.txt"))
-    while(fis.available() > 0) {
-      fos.write(fis.read)
-    }
+    copyStream(fis, fos)
     fis.close
     fos.close
     
+    /*
     val dos = new DataOutputStream( new BufferedOutputStream(
                  new FileOutputStream( new File("textFile") )))
     for(i <- 65 to 90) {
       dos.writeInt(i)
     }
+    dos.writeDouble(math.Pi)
+    dos.writeBoolean(false)
     dos.close
     
+    val dis = new DataInputStream( new BufferedInputStream(
+                 new FileInputStream( new File("textFile") )))
+    for(x <- 1 to 26) {
+      println(dis.readInt)
+    }
+    println(dis.readDouble)
+    println(dis.readBoolean)
+    dis.close
+    */
   }
   
   
